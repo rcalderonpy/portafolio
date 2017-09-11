b52b050732d013a562a062dd3cc6892904e7b67a
Rodrigo Calderón <estudiosimons@tigo.com.py>
2017-09-11 00:19:10 -0400

    Primera Version

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -23,7 +23,7 @@ export class ItemComponent  {
 
               this.cod = parametros['id'];
               this.producto = res.json();
-              console.log(this.producto);
+              // console.log(this.producto);
             })
     })
   }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -5,6 +5,7 @@ import { Http } from '@angular/http';
 export class ProductosService {
 
   productos:any[]=[];
+  productos_filtrado:any[]=[];
   cargando:boolean = false;
 
   constructor( private http:Http ) {
@@ -15,18 +16,54 @@ export class ProductosService {
     return this.http.get(`https://paginaweb-dde1c.firebaseio.com/productos/${cod}.json`);
   }
 
+  public buscar_producto(termino:string){
+    // console.log("Buscando producto");
+    // console.log(this.productos.length);
+
+    if(this.productos.length === 0 ){
+      this.cargar_productos().then(()=>{
+        // termino carga
+      });
+    } else {
+        this.filtrar_productos(termino);
+    }
+
+
+  }
+
+  private filtrar_productos(termino:string){
+    this.productos_filtrado = [];
+    termino = termino.toLowerCase();
+    // console.log(termino);
+
+
+    this.productos.forEach(prod=>{
+
+      if(prod.categoria.indexOf(termino) >=0 || prod.titulo.toLowerCase().indexOf(termino) >=0 ){
+        this.productos_filtrado.push(prod);
+        // console.log(prod);
+      }
+    })
+  }
+
   public cargar_productos(){
 
     this.cargando = true;
-    this.http.get('https://paginaweb-dde1c.firebaseio.com/productos_idx.json')
-        .subscribe( res => {
-          // console.log(res.json());
-          // setTimeout( ()=>{
-            this.cargando = false;
-            this.productos = res.json();
 
-          // }, 1500)
-        })
+    let promesa = new Promise((resolve, reject)=>{
+
+      this.http.get('https://paginaweb-dde1c.firebaseio.com/productos_idx.json')
+      .subscribe( res => {
+        // console.log(res.json());
+        // setTimeout( ()=>{
+        this.cargando = false;
+        this.productos = res.json();
+        resolve();
+
+        // }, 1500)
+      });
+    });
+    return promesa;
 
   }
 
